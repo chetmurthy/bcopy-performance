52c06667c65a3693422794220a8e99e92cc25a10
Chet Murthy <Chetan.Murthy@ibm.com>
2022-07-02 22:09:54 -0500

    meh

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(unused_imports)]
+use std::mem::size_of ;
 use num_complex::Complex64;
 use std::cmp::min;
 use std::time::Instant;
@@ -7,66 +8,109 @@ use std::io;
 use std::path::Path;
 use rayon::prelude::*;
 
-use si_scale::helpers::{seconds, number_};
+use si_scale::helpers::{seconds, number_, bibytes1};
 use conv::prelude::* ;
 
-    pub type RowContents = (Vec<u64>, Vec<Complex64>) ;
+pub struct RowContents {
+    pub indices : Vec<u64>,
+    pub data : Vec<Complex64>,
+}
 
-    pub fn append_rc_slot0(lhs : &mut RowContents, colv : &[u64], datav : &[Complex64]) {
-	colv.iter().for_each(|v| lhs.0.push(*v)) ;
+impl RowContents {
+    pub fn capacity(size : usize) -> RowContents {
+	let mut indices : Vec<u64> = Vec::with_capacity(size) ;
+	let mut data : Vec<Complex64> = Vec::with_capacity(size) ;
+	RowContents { indices, data }
     }
 
-    pub fn append_rc_slot1(lhs : &mut RowContents, colv : &[u64], datav : &[Complex64]) {
-	datav.iter().for_each(|v| lhs.1.push(*v)) ;
+    pub fn new(size : usize, colval : u64, dataval : Complex64) -> RowContents {
+	let mut indices : Vec<u64> = vec![colval; size] ;
+	let mut data : Vec<Complex64> = vec![dataval; size] ;
+	RowContents { indices, data }
     }
 
-    pub fn append_rc(lhs : &mut RowContents, colv : &[u64], datav : &[Complex64]) {
-	append_rc_slot0(lhs, colv, datav);
-	append_rc_slot1(lhs, colv, datav);
+    pub fn append(&mut self, other : &RowContents) -> () {
+	other.indices.iter().for_each(|v| self.indices.push(*v)) ;
+	other.data.iter().for_each(|v| self.data.push(*v)) ;
     }
 
-    pub fn append_rc1(lhs : &mut RowContents, colv : &[u64], datav : &[Complex64]) {
-	lhs.0.copy_from_slice(colv) ;
-	lhs.1.copy_from_slice(datav) ;
+    pub fn clear(&mut self) -> () {
+	self.indices.clear() ;
+	self.data.clear() ;
     }
+
+/*
+pub fn append_rc1(lhs : &mut RowContents, colv : &[u64], datav : &[Complex64]) {
+    lhs.0.copy_from_slice(colv) ;
+    lhs.1.copy_from_slice(datav) ;
+}
+*/
+}
 
 pub fn hack_make_row(size : usize, rowind : u64)
 		-> RowContents
 {
     let recoeff = f64::value_from(rowind).unwrap() ;
     let coeff = Complex64::new(recoeff, 0.0) ;
-    let mut colw = vec![rowind; size] ;
-    let mut dataw = vec![coeff; size] ;
-    (colw, dataw)
+    RowContents::new(size, rowind, coeff)
 }
 
 
-pub fn hack_chunks(count : usize,
+pub fn hack_chunks(iterations : usize,
+		   nrows : usize,
 		   size : usize,
 ) -> () {
 
     let debug = true ;
     let timings = true ;
 
-    let now = Instant::now();
+    if timings { println!("START hack_chunks(iterations={}, nrows={}, size={})",
+			  iterations, nrows, size) ; }
 
-    if timings { println!("START hack_chunks({}, {}): {}",
-			  count, size,
-			  seconds(now.elapsed().as_secs_f64())) ; }
+    let v_rc = {
+	let setup = Instant::now();
+	let v_rc : Vec<RowContents> =
+	    (0..nrows).into_par_iter()
+	    .map(|i| hack_make_row(size, i as u64))
+	    .collect() ;
+	let elapsed = setup.elapsed().as_secs_f64() ;
+	let nnz = nrows * size ;
+	let nbytes = nnz * (size_of::<u64>() + size_of::<Complex64>()) ;
+	let f_nbytes = f64::value_from(nbytes).unwrap() ;
+	let bytespersec = f_nbytes / elapsed ;
+	if timings { println!("AFTER SETUP hack_chunks: nnz={}, {} bytes/sec {}",
+			      number_(f64::value_from(nnz).unwrap()),
+			      bibytes1(bytespersec),
+			      seconds(elapsed)) ; }
+	v_rc
+    } ;
 
-    let mut nnz = 0 as usize ;
-    let rc : RowContents = hack_make_row(size, count as u64) ;
-    let mut indices : Vec<u64> = Vec::with_capacity(size) ;
-    let mut data : Vec<Complex64> = Vec::with_capacity(size) ;
-    let mut dst_rc = (indices, data) ;
-    for i in 0..count {
-	nnz += size ;
-	append_rc(&mut dst_rc, &rc.0[..], &rc.1[..]) ;
-	dst_rc.0.clear() ;
-	dst_rc.1.clear() ;
+
+    let mut v_dst_rc : Vec<RowContents> =
+	(0..nrows).into_iter()
+	.map(|i| RowContents::capacity(size))
+	.collect() ;
+
+    {
+	let copying = Instant::now();
+	v_rc.par_iter()
+	    .zip(v_dst_rc.par_iter_mut())
+	    .for_each(|(rc,dst_rc)| {
+		for i in 0..iterations {
+		    dst_rc.append(&rc) ;
+		    dst_rc.clear()
+		}
+	    }) ;
+
+	let elapsed = copying.elapsed().as_secs_f64() ;
+	let nnz = iterations * nrows * size ;
+	let nbytes = nnz * (size_of::<u64>() + size_of::<Complex64>()) ;
+	let f_nbytes = f64::value_from(nbytes).unwrap() ;
+	let bytespersec = f_nbytes / elapsed ;
+
+	if timings { println!("AFTER COPYING hack_chunks: nnz={}, {} bytes/sec {}",
+			      number_(f64::value_from(nnz).unwrap()),
+			      bibytes1(bytespersec),
+			      seconds(elapsed)) ; }
     }
-
-    if timings { println!("AFTER CHUNKS hack_chunks: nnz={} {}",
-			  number_(f64::value_from(nnz).unwrap()),
-			  seconds(now.elapsed().as_secs_f64())) ; }
 }
